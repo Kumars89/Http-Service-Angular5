517f01d214993a402fc4ce49eb89ea3fac25f714
Kumars89 <kumar.yashwantesh89@gmail.com>
2018-10-23 08:28:53 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'http-angular5';
+  constructor(private http: Http) { }
+   httpdata;
+   // tslint:disable-next-line:use-life-cycle-interface
+   ngOnInit() {
+      this.http.get('http://jsonplaceholder.typicode.com/users').
+      map(
+         (response) => response.json()
+      ).
+      subscribe(
+         (data) => {this.displaydata(data); }
+      );
+   }
+   displaydata(data) {this.httpdata = data; }
+}
